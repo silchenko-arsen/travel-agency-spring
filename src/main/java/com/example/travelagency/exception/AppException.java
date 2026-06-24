feb2a2dc0566889f29e81c64b1cf518ad004a6ae
arsenfurte <arsenfurte@gmail.com>
2026-06-24 19:01:36 +0300

    delete unused code

--- src/main/java/com/example/travelagency/exception/AppException.java
+++
@@ -1,7 +0,0 @@
-package com.example.travelagency.exception;
-
-public abstract class AppException extends RuntimeException {
-    protected AppException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/travelagency/exception/NotFoundException.java
+++ src/main/java/com/example/travelagency/exception/NotFoundException.java
@@ -1,6 +1,6 @@
 package com.example.travelagency.exception;
 
-public class NotFoundException extends AppException {
+public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
     }
